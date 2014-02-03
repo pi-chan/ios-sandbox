87805cd5828c559ee666d347bd8fcacb8a85987f
xoyip <xoyip@piyox.info>
2014-02-03 11:41:49 +0900

    add tips project

---
+++ Tips/Tips/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tips
+//
+//  Created by Hiromasa OHNO on 2014/02/03.
+//  Copyright (c) 2014年 xoyip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
